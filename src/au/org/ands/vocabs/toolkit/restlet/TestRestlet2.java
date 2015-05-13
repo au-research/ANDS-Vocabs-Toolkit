b16e6f291ae4e24b93e5c6718bf822a33f0b4014
Richard Walker <Richard.Walker@ands.org.au>
2015-05-13 14:52:29 +1000

    Test generating a 503 status

--- src/au/org/ands/vocabs/toolkit/restlet/TestRestlet2.java
+++ src/au/org/ands/vocabs/toolkit/restlet/TestRestlet2.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 /** Testing restlet. */
 @Path("testing2")
@@ -23,6 +25,18 @@ public class TestRestlet2 {
     @GET
     public final String getMessageJson() {
         return "{\"hello\":\"Hello JSON!\"}";
+        }
+
+    /** getException.
+     * This shows how to return a status code 503 "service unavailable"
+     * and also some content.
+     * @return the message. */
+    @Path("except")
+    @Produces(MediaType.TEXT_PLAIN)
+    @GET
+    public final Response getExcept() {
+        return Response.status(Status.SERVICE_UNAVAILABLE).
+                entity("Hi there").build();
         }
 
 }
