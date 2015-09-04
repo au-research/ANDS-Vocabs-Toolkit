8f7d71d6c0a545d6bd6d935c28769a7dd740158d
Richard Walker <Richard.Walker@ands.org.au>
2015-09-04 12:44:55 +1000

    Fix error in Versions class comment.

--- src/main/java/au/org/ands/vocabs/toolkit/db/model/Versions.java
+++ src/main/java/au/org/ands/vocabs/toolkit/db/model/Versions.java
@@ -14,7 +14,7 @@ import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 
 /**
- * VersionsUtils model class.
+ * Versions model class.
  */
 @Entity
 @Table(name = "versions",
