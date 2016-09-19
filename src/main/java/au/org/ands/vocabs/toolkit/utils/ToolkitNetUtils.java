e6ac640152fad63c0784113a81f883a387c180bd
Richard Walker <Richard.Walker@ands.org.au>
2016-09-19 16:29:16 +1000

    Add support for a Client that does not follow redirects
    In ToolkitNetUtils, add a getClientNoRedirects() method
    that will return a Client that does not follow redirects.
    Implement it in a similar way to the existing getClient(),
    and ensure that the underlying Client object is shut
    down during context shutdown.

--- src/main/java/au/org/ands/vocabs/toolkit/utils/ToolkitNetUtils.java
+++ src/main/java/au/org/ands/vocabs/toolkit/utils/ToolkitNetUtils.java
@@ -5,26 +5,45 @@ package au.org.ands.vocabs.toolkit.utils;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
+import org.glassfish.jersey.client.ClientProperties;
+
 /** Utility methods for working with the network. */
 public final class ToolkitNetUtils {
 
-    /** A shared Client resource, initialized on class loading. */
-    private static Client client = ClientBuilder.newClient();
+    /** A shared Client resource, initialized on class loading.
+     * Follows redirects. */
+    // According to the documentation, FOLLOW_REDIRECTS is the default.
+    // Specify it anyway, just in case that changes.
+    private static Client client = ClientBuilder.newClient().
+            property(ClientProperties.FOLLOW_REDIRECTS, false);
+
+    /** A shared Client resource, initialized on class loading.
+     * Does not follow redirects. */
+    private static Client clientNoRedirects = ClientBuilder.newClient().
+            property(ClientProperties.FOLLOW_REDIRECTS, false);
 
     /** Private constructor for a utility class. */
     private ToolkitNetUtils() {
     }
 
-    /** Get the shared Client resource.
+    /** Get the shared Client resource that follows redirects.
      * @return The shared Client resource.
      */
     public static Client getClient() {
         return client;
     }
 
+    /** Get the shared Client resource that does not follow redirects..
+     * @return The shared Client resource.
+     */
+    public static Client getClientNoRedirects() {
+        return clientNoRedirects;
+    }
+
     /** Prepare for shutdown. Call this only in webapp context shutdown! */
     public static void doShutdown() {
         client.close();
+        clientNoRedirects.close();
     }
 
 }
