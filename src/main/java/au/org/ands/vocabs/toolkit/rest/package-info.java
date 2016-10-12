cae23a9b0e8e3598ce5e458b22a2e3ebf1ef9776
Richard Walker <Richard.Walker@ands.org.au>
2016-10-12 16:35:06 +1100

    Update package-info.java for rest package
    The change to the comment in package-info.java was deliberately done
    in this separate commit, as otherwise git marked this file as
    a deletion/new file, rather than as a renaming of the file.

--- src/main/java/au/org/ands/vocabs/toolkit/rest/package-info.java
+++ src/main/java/au/org/ands/vocabs/toolkit/rest/package-info.java
@@ -1,3 +1,3 @@
 /** See the file "LICENSE" for the full license governing this code. */
-/** Classes that implement restlets. */
+/** Classes that implement REST web resources. */
 package au.org.ands.vocabs.toolkit.rest;
