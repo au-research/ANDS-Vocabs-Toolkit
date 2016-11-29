355ce5145e75ae67ab58c8f6232b5e8dc16958f3
Richard Walker <Richard.Walker@ands.org.au>
2016-11-30 09:37:17 +1100

    CC-1847: Have PP nightly backup not overwrite Toolkit log
    The nightly PoolParty backup script does not specify a separate
    logging configuration, so it uses the Toolkit's own logging
    configuration. When the backup starts, it overwrites the existing
    Toolkit log file. So if you look in the
    ands-vocabs-toolkit.2016-... files from 2016-06-01 onwards, it looks
    like the Toolkit hasn't done anything.
    
    Now, specify a separate logging config, and have the backup
    script use it.

--- scripts/backup_poolparty.sh
+++ scripts/backup_poolparty.sh
@@ -8,7 +8,13 @@
 : ${catalina_base:=~tomcat}
 : ${TOOLKIT_ROOT:=~tomcat/webapps-ands/vocabtoolkit}
 
-java -Dcatalina.base=${catalina_base} \
+# NB, note use of custom logback configuration.
+# It must not allow any logging to go to the normal Toolkit log!
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+java -Dlogback.configurationFile=$SCRIPTPATH/logback-backup_poolparty.xml \
+     -Dcatalina.base=${catalina_base} \
      -cp $TOOLKIT_ROOT/WEB-INF/lib/*:$TOOLKIT_ROOT/WEB-INF/classes \
      au.org.ands.vocabs.toolkit.provider.backup.PoolPartyBackupProvider \
      "$@"
