287d4f95de3d88bb3978c93af096b9895c751b23
Richard Walker <Richard.Walker@ands.org.au>
2017-01-24 09:38:12 +1100

    First draft of JAXB schemas and code generation for registry
    Add a first draft of JAXB schemas for the registry API
    and the registry database, with code generation in
    build.xml.

---
+++ src/main/java/au/org/ands/vocabs/registry/enums/package-info.java
@@ -0,0 +1,4 @@
+/** See the file "LICENSE" for the full license governing this code. */
+/** Enumerated types for registry entities and the registry schema.
+ * The code for the classes in this package is automatically generated. */
+package au.org.ands.vocabs.registry.enums;
