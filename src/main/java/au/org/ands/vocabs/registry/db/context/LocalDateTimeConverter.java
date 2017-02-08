a4609e61b506498c9c19b96d55bedb18ad380021
Richard Walker <Richard.Walker@ands.org.au>
2017-02-08 17:07:10 +1100

    Add registry LocalDateTimeConverter class
    Define a registry version of the LocalDateTimeConverter
    and make sure it gets added to the persistence unit's JAR file.

---
+++ src/main/java/au/org/ands/vocabs/registry/db/context/LocalDateTimeConverter.java
@@ -0,0 +1,46 @@
+/** See the file "LICENSE" for the full license governing this code. */
+
+package au.org.ands.vocabs.registry.db.context;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/** Converter between {@link java.sql.Timestamp}
+ *  and {@link java.time.LocalDateTime} values that are in UTC.
+ *  Automatically applied by JPA.
+ *  NB: this currently requires Hibernate, and some Hibernate-specific
+ *  configuration:
+ *  {@code hibernate.jdbc.time_zone=UTC}
+ */
+@Converter(autoApply = true)
+public class LocalDateTimeConverter implements
+    AttributeConverter<LocalDateTime, Timestamp> {
+
+    /** Converts a LocalDateTime value that represents a UTC date/time
+     * into a Timestamp in UTC that can be stored in the database.
+     */
+    @Override
+    public Timestamp convertToDatabaseColumn(
+            final LocalDateTime attribute) {
+        if (attribute == null) {
+            return null;
+        }
+        return Timestamp.from(attribute.toInstant(ZoneOffset.UTC));
+    }
+
+    /** Converts a database Timestamp value that represents a UTC date/time
+     * into a LocalDateTime that represents a UTC date/time.
+     */
+    @Override
+    public LocalDateTime convertToEntityAttribute(
+            final Timestamp dbData) {
+        if (dbData == null) {
+            return null;
+        }
+        return LocalDateTime.ofInstant(dbData.toInstant(), ZoneOffset.UTC);
+    }
+}
