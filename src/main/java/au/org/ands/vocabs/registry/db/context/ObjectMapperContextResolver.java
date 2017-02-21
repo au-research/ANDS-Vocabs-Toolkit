4e33b5a7d455573b92d8361ccf4535916321099b
Richard Walker <Richard.Walker@ands.org.au>
2017-02-22 09:10:54 +1100

    Add a JAX-RS ContextResolver to customize JSON generation
    We want to customize the way Jackson does serialization of
    JSON data. In particular, we want it to respect JAXB
    annotations. At the same time, we take the opportunity
    to exclude null values from generated objects, and to
    take only getters into account when looking for properties.

---
+++ src/main/java/au/org/ands/vocabs/registry/db/context/ObjectMapperContextResolver.java
@@ -0,0 +1,51 @@
+package au.org.ands.vocabs.registry.db.context;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+
+/** Custom Jackson {@link ObjectMapper} provider for JSON deserialiation.
+ * The customizations applied are:
+ * (a) JAXB annotations are taken into account; (b) only non-null values
+ * are included (c) only properties with getter methods are taken into account.
+ * Note that despite (c), Jackson uses reflection to get values, rather
+ * than calling getter methods. The difference is apparent for JAXB-generated
+ * properties of List types, in which the getter method initializes the value
+ * to an empty list, if the value is null.
+ */
+@Provider
+public class ObjectMapperContextResolver
+    implements ContextResolver<ObjectMapper> {
+
+    /** Singleton ObjectMapper for JSON generation. It is OK for
+     * this to be a singleton, as ObjectMappers are thread-safe.
+     * The value is customized in a static block. */
+    private static final ObjectMapper OBJECTMAPPER = new ObjectMapper();
+
+    static {
+        JaxbAnnotationModule module = new JaxbAnnotationModule();
+        OBJECTMAPPER.registerModule(module);
+        OBJECTMAPPER.setSerializationInclusion(Include.NON_NULL);
+        // For now, do serialize empty lists as empty lists. Revisit
+        // later, if necessary.
+//        OBJECTMAPPER.configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS,
+//                false);
+        OBJECTMAPPER.setVisibility(PropertyAccessor.FIELD, Visibility.NONE);
+    }
+
+    /** Returns the (singleton) ObjectMapper instance to be used for
+     * JSON deserialization.
+     * @return The singleton ObjectMapper instance.
+     */
+    @SuppressWarnings("checkstyle:DesignForExtension")
+    @Override
+    public ObjectMapper getContext(final Class<?> type) {
+        return OBJECTMAPPER;
+    }
+
+}
